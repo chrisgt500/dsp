13c1888de37ea9136fafb14aba87ae0778113c20
Sean Turner <sean.turner1@maine.edu>
2016-01-27 17:29:18 -0500

    initial c files

---
+++ lab1/running_mean.c
@@ -0,0 +1,16 @@
+/*!
+ * @file
+ * @brief ECE 486 Lab 1 running_mean
+ *
+ * @author ECE486 Lab Group 9
+ * @author Colin Leary, Forrest Smith, Sean Turner
+ *
+ * @date Jan 27 2016
+ *
+ */
+ 
+
+#include "running_mean.h"
+#include <stdlib.h>
+
+
---
+++ lab1/running_mean.h
@@ -0,0 +1,24 @@
+/*!
+ * @file
+ * @brief ECE 486 Lab 1 running_mean
+ *
+ * @author ECE486 Lab Group 9
+ * @author Colin Leary, Forrest Smith, Sean Turner
+ *
+ * @date Jan 27 2016
+ *
+ */
+ 
+ #ifndef RUNNING_MEAN_H
+ #define RUNNING_MEAN_H
+ 
+/*!
+ * @brief 
+ *
+ * @returns
+ *
+ */
+  
+  
+  
+#endif
