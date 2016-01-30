8b382833fe15242741ec402447ca50e4eaee715e
Colin Leary <colin.leary@maine.edu>
2016-01-30 17:12:34 -0500

    Added some Doxygen comments.

--- lab1/running_mean.h
+++ lab1/running_mean.h
@@ -8,12 +8,12 @@
  * @date Jan 27 2016
  *
  */
- 
+
  #ifndef RUNNING_MEAN_H
  #define RUNNING_MEAN_H
- 
+
 /*!
- * @brief 
+ * @brief
  *
  * @returns
  *
@@ -24,10 +24,38 @@ struct s {
     int blocksize;
     float *data_array;
 }
-  
-void memalloc(int bs, int n);
-void shift(struct s *data_props, float start);
+
+
+/*!
+ * @brief Set number of elements to average over, the blocksize and initialize
+ * the data array.
+ *
+ * @returns 	On return, the data_props struct elements are initialized.
+ */
+void memalloc(
+	int bs,		//!< [in] The blocksize of the data to be averaged over.
+	int n		//!< [in] The number of elements to be averaged over.
+);
+
+/*!
+ * @brief Shift each element in data_props->data_array to the right by one.
+ *
+ * The last element of the array is destroyed by the shifting process and a new			
+ * element is placed at the beginning of the array
+ *
+ * @returns		On return, all elements of the array @a data_props->data_array
+ *				have their indeces increased by 1. The last element is lost and 
+ *				the first element is replaced by @a start.
+ */
+void shift(
+	struct s *data_props,	//!< [in,out] Pointer to a struct which holds 
+							//!< blocksize and the array to be shifted.
+	float start				//!< [in] Value to be placed into first element in
+							//!< the array.
+);
+
 float mean(struct s *data_props);
 float * calc_running_mean(float *x, struct s *data_props);
-  
+void memclean(struct s *data_props);
+
 #endif
