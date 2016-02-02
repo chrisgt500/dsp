ed653fff408d55534ac01c2d45965fa6983f118a
Sean Turner <sean.turner1@maine.edu>
2016-02-02 18:16:27 -0500

    All test cases are done and verified. Just needs more comments

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -16,9 +16,12 @@
 int main(void){
   int M, blocksize, i, j, k, iterations;  //initialize variables
   struct s *data;
-  M = 2;
+  
+  /* There are 3 scenarios: M > blocksize, M < blocksize, and M = blocksize */
+  // M < blocksize:
+  M = 3;
   blocksize = 5;
-  iterations = 10;
+  iterations = 5;
   
   float x[iterations][blocksize];
   float *y[blocksize];
@@ -39,14 +42,80 @@ int main(void){
   	y[i] = calc_running_mean(x[i], data);
   }
  
-for (i=0; i <iterations; i++) {             //prints the result
+for (i=0; i <iterations; i++) {             //prints the result and frees the result memory
     for (j=0; j<blocksize; j++) {
         printf("%f ",y[i][j]);
     }
+    free(y[i]);
     printf("\n");
   }
-  	  
- memclean(data);
+
+ memclean(data);    //free the memory from the struct
+ 
+// M > blocksize
+printf("\n\n"); 
+   M = 10;
+  blocksize = 5;
+  iterations = 5;
+  
+
+  
+  k = 1;                    //initialize testing vectors (iteration number of blocks, of size 10, containing numbers 1:iterations*blocksize)
+  for (i=0; i <iterations; i++) {
+    for (j=0; j<blocksize; j++) {
+      x[i][j] = k;
+      k++;
+    }
+  }
+  
+    data = memalloc(blocksize, M);    //dynamically allocates memory for a struct that holds blocksize, M, and current data array
+  
+  
+  for (i=0; i < iterations; i++) {          //performs the running mean routine, iterations number of times
+  	y[i] = calc_running_mean(x[i], data);
+  }
+ 
+for (i=0; i <iterations; i++) {             //prints the result and frees the result memory
+    for (j=0; j<blocksize; j++) {
+        printf("%f ",y[i][j]);
+    }
+    free(y[i]);
+    printf("\n");
+  }
+
+ memclean(data);        //free the memory from the struct
     
+    
+// M = blocksize
+printf("\n\n"); 
+   M = 5;
+  blocksize = 5;
+  iterations = 5;
+
+  
+  k = 1;                    //initialize testing vectors (iteration number of blocks, of size 10, containing numbers 1:iterations*blocksize)
+  for (i=0; i <iterations; i++) {
+    for (j=0; j<blocksize; j++) {
+      x[i][j] = k;
+      k++;
+    }
+  }
+  
+    data = memalloc(blocksize, M);    //dynamically allocates memory for a struct that holds blocksize, M, and current data array
+  
+  
+  for (i=0; i < iterations; i++) {          //performs the running mean routine, iterations number of times
+  	y[i] = calc_running_mean(x[i], data);
+  }
+ 
+for (i=0; i <iterations; i++) {             //prints the result and frees the result memory
+    for (j=0; j<blocksize; j++) {
+        printf("%f ",y[i][j]);
+    }
+    free(y[i]);
+    printf("\n");
+  }
+
+ memclean(data);        //free the memory from the struct
     return 0;
 }
