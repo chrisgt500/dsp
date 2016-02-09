ccb5aa551b29fde33de7b85fa36706996abd7f5e
Sean Turner <sean.turner1@maine.edu>
2016-02-09 13:47:51 -0500

    ece486_fir.c logic is done, needs comments/testing

--- lab2/ece486_fir.c
+++ lab2/ece486_fir.c
@@ -12,30 +12,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ece486_fir.h"
- 
+
 struct FIR_T * init_fir (float *fir_coefs, int n_coef, int blocksize) {
 
 	int i;
 	struct s *fir_data = malloc(sizeof(struct FIR_T));
 	fir_data->M = n_coef;
 	fir_data->blocksize = blocksize;
-	fir_data->data_array = calloc(n_coef, sizeof(float));
+	fir_data->h = calloc(n_coef, sizeof(float));
 
 	for(i=0; i < n_coef; i++) {
+		fir_data->h[i] = fir_coefs[i];  //talk to Colin about this
+	}
 
-		fir_data->data_array[i] = fir_coefs[i];
-
-	}	
-			
 }
 
 void calc_fir (FIR_T *s, float *x, float *y) {
-
+  int n, k;
+  float sum;
+  for (n = 0; n < blocksize; n++){
+    sum = 0;
+    for (k = 0; k < s->M; k++){
+      sum += h[k] * x[n-k];
+    }
+    y[n] = sum;
+  }
 }
 
 void destroy_fir (FIR_T *s) {
-	
+
 	free(fir_data->array_data);
 	free(fir_data);
-		
+
 }
