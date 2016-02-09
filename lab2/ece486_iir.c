00d43f576198af4b8a0bc079e3df267a8325ebd7
Forrest Smith <fwsmith898@gmail.com>
2016-02-09 13:49:15 -0500

    Added function headings to ece486_iir.c

---
+++ lab2/ece486_iir.c
@@ -0,0 +1,28 @@
+/*
+ * @brief ECE 486 Lab 2 ece486_iir.c
+ *
+ * @author ECE486 Lab Group 9
+ * @author Colin Leary, Forrest Smith, Sean Turner
+ *
+ * @date Feb 9 2016
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ece486_iir.h"
+
+
+BIQUAD_T *init_biquad (int sections, float g, float *biquad_coefs, int blocksize) {
+
+}
+
+void calc_biquad (BIQUAD_T *s, float *x, float *y) {
+
+}
+
+void  destroy_biquad (BIQUAD_T *s) {
+
+}
+
+
