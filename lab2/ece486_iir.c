622efb2347e0a2b9dd82cf6ea3ed24c17555fc22
Forrest Smith <fwsmith898@gmail.com>
2016-02-09 14:12:44 -0500

    Added contents of memory allocation and decallocation functions

--- lab2/ece486_iir.c
+++ lab2/ece486_iir.c
@@ -13,8 +13,25 @@
 #include "ece486_iir.h"
 
 
-BIQUAD_T *init_biquad (int sections, float g, float *biquad_coefs, int blocksize) {
-
+struct BIQUAD_T * init_biquad (int sections, float g, float *biquad_coefs, int blocksize) {
+	
+	int totalcoefs, i;
+	totalcoefs = 5*sections;
+	struct BIQUAD_T *s = malloc(sizeof(struct BIQUAD_T));
+	s->sections = sections;
+	s->g = g;
+	s->blocksize = blocksize;
+	s->stored_data[0] = 0;
+	s->stored_data[1] = 0;
+	s->all_coefs = calloc(totalcoefs, sizeof(float));
+	
+	for(i = 0; i < totalcoefs; i++) {
+		s->all_coefs[i] = biquad_coefs[i];
+		if(i < 5) s->current_coefs[i] = biquad_coefs[i];
+	}
+	
+	
+	
 }
 
 void calc_biquad (BIQUAD_T *s, float *x, float *y) {
@@ -22,6 +39,9 @@ void calc_biquad (BIQUAD_T *s, float *x, float *y) {
 }
 
 void  destroy_biquad (BIQUAD_T *s) {
+
+	free(s->all_coefs);
+	free(s);
 
 }
 
