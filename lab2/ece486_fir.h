57cf6b48ea2c6385e75f69fad246ef58015e2350
Sean Turner <sean.turner1@maine.edu>
2016-02-09 14:37:54 -0500

    fir test needs testing

--- lab2/ece486_fir.h
+++ lab2/ece486_fir.h
@@ -12,42 +12,44 @@
 #ifndef ECE486_IIR_H
 #define ECE486_IIR_H
 
+/*!
+  * @brief A structure for holding FIR_T components
+  *
+  * This structure holds everything needed to use calc_fir
+*/
 struct FIR_T{
-  int M;
-  int blocksize;
-  float *data_array;
+  int M; //!< This is number of coefficients of h
+  int blocksize; //!< This is the number of data values passed in at once
+  float *h; //!< This is a vector containing the coefficients of an impulse response
 };
 
 /*!
-  * @brief
+  * @brief The function
   *
   * @returns
 */
-
 FIR_T * 	init_fir (
   float *fir_coefs, //!<
   int n_coef, //!<
   int blocksize //!<
  );
 
- /*!
+/*!
+ * @brief
+ *
+ * @returns
+*/
+void 	calc_fir (
+  FIR_T *s, //!<
+  float *x, //!<
+  float *y //!<
+);
+
+/*!
   * @brief
   *
   * @returns
-  */
-
- void 	calc_fir (
-   FIR_T *s, //!<
-   float *x, //!<
-   float *y //!<
- );
-
- /*!
-  * @brief
-  *
-  * @returns
-  */
-
+*/
 void 	destroy_fir (
   FIR_T *s //!<
 );
--- lab2/fir_test.c
+++ lab2/fir_test.c
@@ -20,17 +20,26 @@
 int main(void)
 {
   float *input, *output;
+  int blocksize;
+  struct FIR_T *data;
+  float h[10] ={2.2044, 1.3420, .8827, -.7646,-1.3219,-.0629,1.2442,.8185,-.7088,-1.2257};
+
 
   initialize(FS_50K, MONO_IN, MONO_OUT);
+  blocksize = getblocksize();
 
-  //init_fir
+  input = (float *)malloc(sizeof(float)*blocksize);
+  output = (float *)malloc(sizeof(float)*blocksize);
+
+  data = init_fir(*h,10,blocksize);
+
   while(1){
     getblock(input);
     DIGITAL_IO_SET();
-    //calc_fir
+    calc_fir(data,input,output);
     DIGITAL_IO_RESET();
     putblock(output);
   }
-  //destroy_fir
+  destroy_fir(data);
   return 0;
 }
