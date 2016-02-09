c09d9ff8b7a503386e0d9c61925bf4ee0c771a1a
Forrest Smith <fwsmith898@gmail.com>
2016-02-09 12:42:09 -0500

    Added functions to ece486_fir.c

--- lab2/ece486_fir.c
+++ lab2/ece486_fir.c
@@ -1,2 +1,26 @@
- 
+/*!
+ * @file
+ * @brief ECE 486 Lab 2 ece486_fir.c
+ *
+ * @author ECE486 Lab Group 9
+ * @author Colin Leary, Forrest Smith, Sean Turner
+ *
+ * @date Feb 9 2016
+ *
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "ece486_fir.h"
+ 
+FIR_T * init_fir (float *fir_coefs, int n_coef, int blocksize) {
+
+}
+
+void calc_fir (FIR_T *s, float *x, float *y) {
+
+}
+
+void destroy_fir (FIR_T *s) {
+
+}
