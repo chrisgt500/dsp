a1162006372b30728f9fb482969da5f6ddf9f852
Sean Turner <sean.turner1@maine.edu>
2016-02-09 14:19:12 -0500

    doxygen commented ece486_iir.h

--- lab2/ece486_iir.h
+++ lab2/ece486_iir.h
@@ -12,48 +12,62 @@
 #ifndef ECE486_IIR_H
 #define ECE486_IIR_H
 
+/*!
+ * @brief A structure for holding BIQUAD_T components
+ *
+ * The BIQUAD_T structure is used to hold all of the
+ * values needed to execute calc_biquad
+ *
+ */
 struct BIQUAD_T{
-  float g;
-  int sections;
-  int blocksize;
-  float stored_data[2];
-  float current_coefs[5];
-  float *all_coefs;
+  float g; //!< The gain value
+  int sections; //!< How many 2nd order filters to cascade
+  int blocksize; //!< How many data values are passed at once
+  float stored_data[2]; //!< Holds two values needed to calculate the transposed direct form II
+  float current_coefs[5]; //!< Only holds the current difference equation coefficients
+  float *all_coefs; //!< Holds all of the difference equation coefficients
 };
 
 /*!
-  * @brief
+  * @brief A function for initializing the BIQUAD_T structure
   *
-  * @returns
+  * This function takes several parameters and dynamically allocates the required
+  * memory. Once initialized, it contains everything to call calc_biquad
+  *
+  * @returns On return, the allocated and filled in struct is returned
 */
-
 BIQUAD_T *init_biquad (
-  int sections, //!<
-  float g, //!<
-  float *biquad_coefs, //!<
-  int blocksize //!<
+  int sections, //!< How many 2nd order filters to cascade
+  float g, //!< The gain value
+  float *biquad_coefs, //!< A pointer to the array of in the form [b10, b11, b12
+                       //!< a11, a12, b20, ...]
+  int blocksize //!< How many data values are passed at once
  );
 
  /*!
-  * @brief
+  * @brief    A function to perform a an IIR filter on a stream of data
   *
-  * @returns
+  * This function performs the transposed direct form II version of an IIR filter
+  * on a stream of data.
+  *
+  * @returns  On return, all elements in the y pointer are a filtered version of x.
+  *           The struct BIQUAD_T still holds all of the necessary information
   */
-
  void 	calc_biquad (
-   BIQUAD_T *s, //!<
-   float *x, //!<
-   float *y //!<
+   BIQUAD_T *s, //!< [in,out] Pointer to the struct that holds all the needed information
+   float *x, //!< [in] The data stream to filter, in size of blocksize
+   float *y //!< [out] The filter data, after being filtered through the
+            //!< transposed direct form II structure
  );
 
  /*!
-  * @brief
+  * @brief    A function to cleanly deallocate memory from the struct
   *
-  * @returns
+  * The function first frees the memory from the dynamically allocated array in
+  * the struct, and then frees the memory for the struct itself.
   */
-
 void 	destroy_biquad (
-  BIQUAD_T *s //!<
+  BIQUAD_T *s //!< This is struct that has memory to be freed
 );
 
 
