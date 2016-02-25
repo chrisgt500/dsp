6f43172cf185e1e4eeea1499d2956085f1c71a7d
Sean Turner <sean.turner1@maine.edu>
2016-02-25 15:40:17 -0500

    task 3 updated

---
+++ lab3/lab3_task3.m
@@ -0,0 +1,15 @@
+function [] = lab3_task3(sos, g)
+%LAB3_TASK3 Summary of this function goes here
+%   Detailed explanation goes here
+
+fprintf('float g = %f;\n',g);
+fprintf('int sections = %d;\n', size(sos,1));
+fprintf('float coefs[%d] = {\n',5*size(sos,1));
+for i = 1:size(sos,1);
+    for j = [1 2 3 5 6]
+        fprintf('%f, ',sos(i,j)); 
+    end
+    fprintf('\n');
+end
+fprintf('\b\b\b\n};');
+
