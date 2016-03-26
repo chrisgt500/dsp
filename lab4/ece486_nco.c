f96f0a5547dee1dba07b7c1fdabd04cd81228fb6
Sean Turner <sean.turner1@maine.edu>
2016-03-26 13:31:23 -0400

    updated again

--- lab4/ece486_nco.c
+++ lab4/ece486_nco.c
@@ -1,98 +1,62 @@
-/*****************************************************************
-
-
- file: ece486_nco.c
- Description:  Subroutines to implement numerically controlled oscillators
-        Multiple subroutine calls are used to create sample sequences
-        for sinusoidal functions with programmable frequency and phase.
-            y(n) = cos(2 pi f0 n + theta)
-        
-
- Interface:
-     init_nco() is called once (for each oscillator), and is used to create
-                any required oscillator structures.  This routine handles all
-                array initialiaztion, allocates any required memory,
-                and perform any other required initialization.
-     nco_set_frequency() is used to change the frequency of the nco 
-     nco_set_phase() is used to change the phase of the nco 
-     nco_get_samples() return a block of nco output samples.
-     destroy_nco() is called once at the end of the program, and is used
-                  to de-allocate any memory associated with an nco.
-
-  Function Prototypes and parameters:
-
-        #include "nco.h"
-        NCO_T *init_nco(float f0, float theta);
-           Inputs:
-                f0     Initial NCO discrete-time frequency for the output sample
-                       sequence (in cycles/sample)
-                theta  Inital Phase of the output sample sequence (radians)
-           Returned:
-                The function returns a pointer to a "NCO_T" data type
-                (which is defined in "nco.h")
-        
-        void nco_get_samples(NCO_T *s, float *y, int n_samples);
-           Inputs:
-              s         pointer to initialized NCO_T
-              y         Caller-supplied array to be replaced by NCO output samples
-              n_samples Number of NCO output samples to generate.
-           Returned:
-              On return, y[i] (for i=0,1,...n_samples-1) contains the the next
-              n_samples values of the NCO output sequence.  Subsequent calls to 
-              nco_get_samples() continues to return NCO output samples with no 
-              phase discontinuities from one call to the next. 
-              Example:
-                NCO_T *s;
-                float y1[15],y2[5];
-                s=init_nco(0.11, 0.0);       // set up NCO for .11 cycles/sample
-                nco_get_samples(s, y1, 15);  // Generate samples 0,1, ..., 14 of y(n)
-                nco_get_samples(s, y2, 5);   // Generate samples 15,16,17,18,19 of y(n)
-              
-        void nco_set_frequency(NCO_T *s, float f_new);
-          Inputs:
-             s         pointer to NCO_T
-             f_new     New NCO frequency (in cycles/sample).
-          Returned:
-             The NCO_T structure s is modified so that subsequent calls to nco_get_samples()
-             will operate at frequency f_new (without loss of phase continuity).
-        
-        void nco_set_phase(NCO_T *s, float theta);
-          Inputs:
-             s         pointer to NCO_T
-             theta     New NCO phase.
-          Returned:
-             The NCO_T structure s is modified so that subsequent calls to nco_get_samples()
-             will operate with the phase shift given by theta (in radians)
-        
-        void destroy_nco(NCO_T *s);
-           Inputs:
-                s       pointer to NCO_T, as provided by init_nco()
-           Returned: Any resources associated with the nco "s" are released.
- 
- 
-*******************************************************************/ 
-
 #include "ece486_nco.h"
+#include "math.h"
+
 
 NCO_T *init_nco(float f0, float theta){
-    /*******  ECE486 STUDENTS MODIFY THIS *******/
+	NCO_T *s = malloc(sizeof(NCO_T));
+	s->f0 = f0;
+	s->theta_const = theta;
+	s->theta_temp = 0.0;
+	return s;
 }
 
 void nco_get_samples(NCO_T *s, float *y, int n_samples){
-    /*******  ECE486 STUDENTS MODIFY THIS *******/
+	float index;
+	int i;
+
+	for ( i = 0; i < n_samples; i ++){
+		s->theta_temp = s->theta_temp + 2*pi*(s->f0);
+		index = [(s->theta_temp)+(s->theta_const)]*[512/(2*pi)];
+		index = rint(index)%512;
+		y[i] = cos(index);
+	}
 }
 
 void nco_set_frequency(NCO_T *s, float f_new){
-    /*******  ECE486 STUDENTS MODIFY THIS *******/
+    s->f0 = f_new;
 }
 
 void nco_set_phase(NCO_T *s, float theta){
-    /*******  ECE486 STUDENTS MODIFY THIS *******/
+    s->theta = theta;
 }
 
 void destroy_nco(NCO_T *s){
-    /*******  ECE486 STUDENTS MODIFY THIS *******/
+    free(s);
+	s = NULL;
 }
 
-
-
+float cosine(int index){
+	static const float lookup[128] =
+	{
+		1.000000, 0.999924, 0.999694, 0.999312, 0.998776, 0.998088, 0.997248, 0.996254,
+		0.995109, 0.993811, 0.992361, 0.990759, 0.989006, 0.987101, 0.985045, 0.982839,
+		0.980482, 0.977976, 0.975320, 0.972514, 0.969560, 0.966457, 0.963207, 0.959809,
+		0.956265, 0.952574, 0.948737, 0.944755, 0.940629, 0.936359, 0.931946, 0.927390,
+		0.922692, 0.917853, 0.912873, 0.907754, 0.902497, 0.897101, 0.891567, 0.885898,
+		0.880093, 0.874153, 0.868079, 0.861873, 0.855535, 0.849066, 0.842467, 0.835739,
+		0.828884, 0.821901, 0.814793, 0.807561, 0.800204, 0.792725, 0.785125, 0.777405,
+		0.769566, 0.761610, 0.753536, 0.745348, 0.737045, 0.728630, 0.720103, 0.711466,
+		0.702720, 0.693867, 0.684908, 0.675843, 0.666676, 0.657406, 0.648036, 0.638567,
+		0.629000, 0.619337, 0.609579, 0.599728, 0.589785, 0.579752, 0.569630, 0.559421,
+		0.549126, 0.538748, 0.528287, 0.517745, 0.507124, 0.496425, 0.485651, 0.474802,
+		0.463881, 0.452888, 0.441826, 0.430697, 0.419502, 0.408243, 0.396921, 0.385538,
+		0.374097, 0.362598, 0.351044, 0.339436, 0.327776, 0.316066, 0.304308, 0.292503,
+		0.280654, 0.268761, 0.256827, 0.244854, 0.232844, 0.220798, 0.208718, 0.196606,
+		0.184465, 0.172295, 0.160098, 0.147877, 0.135634, 0.123370, 0.111087, 0.098786,
+		0.086471, 0.074143, 0.061803, 0.049454, 0.037097, 0.024734, 0.012368, 0.000000
+	};
+	if (value < 127) return lookup[value];
+	else if (value >= 127 && value < 255) return -lookup[254-value];
+	else if (value >= 255 && value < 382) return -lookup[value-254];
+	else if (value >= 384) return lookup[511-value];
+}
