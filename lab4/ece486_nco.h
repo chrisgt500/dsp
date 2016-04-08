4c19738a5a6c626db5c952f1e385a2956e228160
Colin Leary <colin.leary@maine.edu>
2016-04-07 21:35:25 -0400

    Commented ece486_nco.h

--- lab4/ece486_nco.h
+++ lab4/ece486_nco.h
@@ -55,15 +55,19 @@ typedef struct{
  *(which is defined in "nco.h").
  */
 NCO_T *init_nco(
-  float f0, //!< Initial NCO discrete-time frequency for the output sample
-            //!<sequence (in cycles/sample)
-  float theta //!<Inital Phase of the output sample sequence (radians)
+  float f0, //!< [in] Initial NCO discrete-time frequency for the output sample
+            //!< sequence (in cycles/sample)
+  float theta //!< [in] Inital Phase of the output sample sequence (radians)
 );
 
+
+
 /*!
- *@Brief 
+ *@Brief A function for calculating the values of an NCO.
  *
- *THIS FUNCTION DOES A THING
+ * This function calculates the index of a cosine lookup table and then uses
+ * that index to obtain a value from the lookup table. The value is written to 
+ * an output buffer.
  *
  *@returns On return, y[i] (for i=0,1,...n_samples-1) contains the the next
  *n_samples values of the NCO output sequence.  Subsequent calls to
@@ -71,61 +75,72 @@ NCO_T *init_nco(
  *phase discontinuities from one call to the next.
  */
 void nco_get_samples(
-  NCO_T *s, //!<pointer to initialized NCO_T
-  float *y, //!<Caller-supplied array to be replaced by NCO output samples
-  int n_samples //!<Number of NCO output samples to generate
+  NCO_T *s, //!< [in, out] pointer to initialized NCO_T
+  float *y, //!< [in] Caller-supplied array to be replaced by NCO output samples
+  int n_samples //!< [in] Number of NCO output samples to generate
 );
 
+
+
 /*!
- *@Brief
+ *@Brief A function to set the frequency of the NCO.
  *
- *THIS FUNCTION DOES A THING
+ * This function modifies the value f0 in the struct to modify the frequency of
+ * the output.
  *
  *@returns The NCO_T structure s is modified so that subsequent calls to nco_get_samples()
  *will operate at frequency f_new (without loss of phase continuity).
  */
-
 void nco_set_frequency(
-  NCO_T *s, //!< pointer to NCO_T
-  float f_new  //!<New NCO frequency (in cycles/sample)
+  NCO_T *s, //!< [in, out] pointer to NCO_T
+  float f_new  //!< [in] New NCO frequency (in cycles/sample)
 );
 
+
+
+
+
 /*!
- *@Brief
+ *@Brief A function to set the phase of the NCO.
  *
- *THIS FUNCTION DOES A THING
+ * This function modifies the value theta in the struct to modify the phase of
+ * the output.
  *
  *@returns The NCO_T structure s is modified so that subsequent calls to nco_get_samples()
  *will operate with the phase shift given by theta (in radians).
  */
-
 void nco_set_phase(
-  NCO_T *s, //!< pointer to NCO_T
-  float theta //!< New NCO phase
+  NCO_T *s, //!< [in, out] pointer to NCO_T
+  float theta //!< [in] New NCO phase
 );
 
+
+
+
 /*!
- *@Brief
+ *@Brief A function to destroy the NCO struct.
  *
- *THIS FUNCTION DOES A THING
+ * This function frees the memory allocated to NCO_T by the init function and
+ * sets the pointers to NULL.
  *
  *@returns Any resources associated with the nco "s" are released.
  */
-
  void destroy_nco(
-   NCO_T *s //!<pointer to NCO_T, as provided by init_nco()
+   NCO_T *s //!< [in, out] pointer to NCO_T, as provided by init_nco()
  );
 
+ 
+
  /*!
-  *@Brief
+  *@Brief A function to return the value of a cosine lookup table.
   *
-  *THIS FUNCTION DOES A THING
+  * This function takes the input parameter and returns the value of a cosine
+  * lookup table at the given index.
   *
   *@returns The function returns a value from the cosine lookup table
   */
-
   float cosine_lookup(
-    uint32_t index //!<index to return a value from the look up table
+    uint32_t index //!< [in] index to return a value from the look up table
   );
 
 
