a373b6a81465936febba0656409778d9444f63ab
Forrest Smith <fwsmith898@gmail.com>
2016-04-16 15:29:58 -0400

    changed task 1 stuff, designed spec'd filter

--- lab5/lab_demo.m
+++ lab5/lab_demo.m
@@ -1,3 +1,4 @@
+%{
 N = 8*1024;
 f = (0:N-1)/N;
 f(N/2+1+1:end) = f(N/2+1+1:end)-1; %%keeps track of negative freq too
@@ -30,3 +31,30 @@ hold on;
 stem(0:M-1,h,'.'); %%transfer function of 
 figure(2);
 plot(f, 20*log10(abs(fft(h,N))));
+%}
+
+N1 = 8*1024;
+f1 = (0:N-1)/N;
+f1(N1/2+1+1:end) = f1(N1/2+1+1:end)-1; %%keeps track of negative freq too
+
+M1 =111;  %%affects filter order
+beta1 = 7.95;
+figure(3); clf;
+patch ([0 .07 .07 0], [.5 .5 -.5 -.5], .9*[1 1 1]); - .07
+patch ([.5 .11 .11 .5], [-100 -100 -80 -80], .9*[1 1 1]); %%specs
+hold on;
+Hr1 = (abs(f1)< .083)  ...         %real transfer using this line for pass band
+    .*(10 .^ 0); 
+
+Hd1 = Hr1.*exp(-j*2*pi*f1*(M1-1)/2); %%desired tranfer
+
+plot(f, 20*log10(abs(Hr1)));  %%plots the gain of real transfer in dB
+axis([ 0 .5 -90 20]);
+
+hd1 = ifft(Hd1);  %%transfer function of desired impulse response
+stem(0:N1-1 , hd1, '.');
+
+h1 = hd1(1:M1).*kaiser(M1, beta1)'; %%windowed impulese
+hold on;
+stem(0:M1-1,h1,'.'); %%transfer function of 
+plot(f1, 20*log10(abs(fft(h1,N1))));
