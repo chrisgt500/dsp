58c28718b3aa85472d7117775857c8e9763dab13
Sean Turner <sean.turner1@maine.edu>
2016-04-16 15:43:17 -0400

    finished Task 2

--- lab5/lab_demo_2.m
+++ lab5/lab_demo_2.m
@@ -1,6 +1,6 @@
-numPeaks = 2;
+numSamples = 512; %% samples of x
 
-%Phase and Magnitude Initializations
+%% Phase and Magnitude Initializations
 A0 = 25.3;
 f0 = .3074;
 theta0 = .1234;
@@ -9,42 +9,51 @@ A1 = 0.2;
 f1 = 0.33;
 theta1 = .5678;
 
-N1 = 512; %% samples
-n = 0:N1-1;
+n = 0:numSamples-1;
 x = A0*cos(2*pi*f0*n+theta0) + A1*cos(2*pi*f1*n+theta1);
+%% FFT
 
-%figure(1);clf;
-%stem(n,x);
+N = 64*numSamples*2; %zero padding much much larger than numSamples
+f = (0:N-1)/N; %normalized padded frequenct
 
+w = kaiser(numSamples, 10)'; %kaiser window function
+H = sum(w); %needed to find amplitude
+X = fft(x.*w,N); %performs the windowed, zero-padded FFT
+dBX = 20*log10(abs(X)); %converts to dB
+[peaks, locations] = findpeaks(dBX(1:end/2), f(1:end/2), 'SortStr','descend'); %finds the all peaks and their locations and sorts them
 
-X = fft(x);
-f = (0:N1-1)/N1;
-figure(2); clf;
+figure(1); clf;
+plot(f(1:end/2), dBX(1:end/2)); %plots the FFT (only half of normalized Frequency)
 
+numPeaks = input('How many peaks do you want to detect? '); %user input for number of peaks
 
-%stem(f,abs(X),'.');
-
-N2 = 64*1024;
-X2 = fft(x, N2);
-f2 = (0:N2-1)/N2;
-hold on;
-plot(f2,abs(X2));
-
-figure(3); clf;
-w = kaiser(512, 8)';
-H = sum(w);
-X3 = fft(x.*w,N2);
-%plot(f2,abs(X3));
-dBX3 = 20*log10(abs(X3));
-[peaks, locations] = findpeaks(dBX3(1:end/2), f2(1:end/2), 'SortStr','descend');
-
-
-plot(f2(1:end/2), dBX3(1:end/2));
-
-detectedMags = zeros(1,numPeaks);
+detectedMags = zeros(1,numPeaks); %preallocation for speed
 detectedFreqs = zeros(1,numPeaks);
 
-for i = 1:numPeaks
-    detectedMags(i) = 10^(peaks(i)/20)*2/H;
+for i = 1:numPeaks %loops over the highest peaks and resturns the associated amplitude and frequenct
+    detectedMags(i) = 10^(peaks(i)/20)*2/H; %converts from dB and then scales by window function
     detectedFreqs(i) = locations(i);
 end
+
+%outputs to the terminal
+detectedMags
+detectedFreqs
+
+%% Useful plots
+% FFT No zero pad, no window
+%{
+plot((0:numSamples-1)/numSamples, abs(fft(x)));
+%}
+
+% FFT Zero pad, no window
+%{
+plot((0:N-1)/N, abs(fft(x,N)));
+%}
+
+% Stem plot no zero pad, no window, no FFT, not normalized
+%{
+stem(0:numSamples-1, x);
+%}
+
+
+
