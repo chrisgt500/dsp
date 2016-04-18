e1ce430dc49231e9bb97c2163aa6fd634655aca0
Colin Leary <colin.leary@maine.edu>
2016-04-17 20:59:44 -0400

    Added some lovely comments

--- lab5/lab5_task1a.m
+++ lab5/lab5_task1a.m
@@ -1,73 +1,5 @@
-%{
-N = 8*1024;
-f = (0:N-1)/N;
-f(N/2+1+1:end) = f(N/2+1+1:end)-1; %%keeps track of negative freq too
-
-M = 81;  %%affects filter order
-beta = 8.2; %%filter shape
-
-Hr = (abs(f) > .135 & abs(f)<.315)  ...         %real transfer using this line for pass band
-    .*(10 .^ ((abs(f) - .15) * 10 / .15  /20)); 
-
-Hd = Hr.*exp(-j*2*pi*f*(M-1)/2); %%desired tranfer
-
-figure(2); clf;
-
-patch ([0 .1 .1 0], [-100 -100 -70 -70], .9*[1 1 1]); %%specs
-patch ( [.35 .5 .5 .35], [-100 -100 -70 -70], .9*[1 1 1]);
-
-patch([.15 .3 .3 .15],[-1 9 11 1], .9*[1 1 1]);
-hold on;
-
-plot(f, 20*log10(abs(Hr)));  %%plots the gain of real transfer in dB
-axis([ 0 .5 -80 20]);
-
-figure(1); clf;
-hd = ifft(Hd);  %%transfer function of desired impulse response
-stem(0:N-1 , hd, '.');
-
-h = hd(1:M).*kaiser(M, beta)'; %%windowed impulese
-hold on;
-stem(0:M-1,h,'.'); %%transfer function of 
-figure(2);
-plot(f, 20*log10(abs(fft(h,N))));
-%}
-%{
-N1 = 8*1024;
-f1 = (0:N1-1)/N1;
-f1(N1/2+1+1:end) = f1(N1/2+1+1:end)-1; %%keeps track of negative freq too
-
-M1 =111;  %%affects filter order
-beta1 = 7.95;
-figure(3); clf;
-patch ([0 .07 .07 0], [.5 .5 -.5 -.5], .9*[1 1 1]);
-patch ([.5 .11 .11 .5], [-100 -100 -80 -80], .9*[1 1 1]); %%specs
-hold on;
-
-
-
-
-Hr1 = (abs(f1)< .083)  ...         %real transfer using this line for pass band
-    .*(10 .^ 0); 
-
-
-
-Hd1 = Hr1.*exp(-j*2*pi*f1*(M1-1)/2); %%desired tranfer
-
-plot(f1, 20*log10(abs(Hr1)));  %%plots the gain of real transfer in dB
-axis([ 0 .5 -90 20]);
-
-hd1 = ifft(Hd1);  %%transfer function of desired impulse response
-stem(0:N1-1 , hd1, '.');
-
-h1 = hd1(1:M1).*kaiser(M1, beta1)'; %%windowed impulese
-hold on;
-stem(0:M1-1,h1,'.'); %%transfer function of 
-plot(f1, 20*log10(abs(fft(h1,N1))));
-%}
-
-%e{
-%% Colin's (and Sean's) Attempt
+%% FIR filter design
+% Authors: Colin Leary, Forrest Smith, Sean Turner
 
 % Specs: less than .1dB ripple in passband, 0 > |f| > 0.07
 %       more than 80dB rejection in stopband, .11 < |f| < 0.5
@@ -95,7 +27,7 @@ hrspec = .017;       %how far Hr should extend beyond spec
 f = (0:N-1)/N;      %specify normalized frequencies
 f(N/2+1+1:end) = f(N/2+1+1:end)-1; %keep track of negative frequencies
 
-stem_logical = 1;
+stem_logical = 0;
 
 %% Plot filter response
 % Generate stop and pass bands
@@ -111,11 +43,13 @@ patch([passlow passhigh passhigh passlow], ...
           .9*[1 1 1]); 
 
 if (passlow == 0) || (passhigh == .5)  %check type of filter
+    %if not bandpass, plot accordingly
     patch([stopbegin stopend stopend stopbegin], ...
           [minreject minreject winlow winlow], ...
           .9*[1 1 1]);
       
 else
+    %if bandpass, plot two stopbands
     patch([0 stopend stopend 0], ...
           [minreject minreject winlow winlow], ...
           .9*[1 1 1]);
@@ -125,25 +59,29 @@ else
 end
 
 %% Calculate real and desired transfer functions
+m = (endgain-startgain)/(passhigh-passlow);     %slope of Hr
 
-m = (endgain-startgain)/(passhigh-passlow);
-
+%Calculate entire line (point-slope formula) and convert from dB
+%Then limit the line to within hrspec
 Hr =  ((abs(f) > (passlow-hrspec)) & (abs(f) < (passhigh+hrspec))) .* ...
         10 .^ (((abs(f) .* m) - (m*passlow) + startgain)/20);
 
+%plot the desired Hr for visualization
 plot(f, 20*log10(abs(Hr)));
 
-Hd = Hr.*exp(-j*2*pi*f*(M-1)/2); %%desired tranfer
+%From the real transfer function, obtain the complete desired transfer function
+Hd = Hr.*exp(-j*2*pi*f*(M-1)/2);
 
+%Perform inverse FFT to obtain impulse response
+hd = ifft(Hd);
 
+%window impulse response to obtain filter response
+h = hd(1:M).*kaiser(M, beta)';
 
-
-hd = ifft(Hd);  %%desired impulse response from desired transfer function
-h = hd(1:M).*kaiser(M, beta)'; %%windowed impulse
-
-figure(1);
+%plot FFT of resulting, windowed filter response
 plot(f, 20*log10(abs(fft(h,N))));
 
+%optionally plot a stem plot of the windowed and non windowed impulse responses
 if(stem_logical)
     figure(2); clf;
     stem(0:N-1 , hd, '.');
