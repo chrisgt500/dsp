055874b3d0b24f5e4db25fedc955b6b374a5b872
Sean Turner <sean.turner1@maine.edu>
2016-04-22 15:51:51 -0400

    wrote a better peak detect script

--- lab6/ece486_fft.c
+++ lab6/ece486_fft.c
@@ -1,16 +1,10 @@
 int peak_detect(float *data, int size, float thresh, int *peaks)
 {
 	int i, count = 0;
-	float r = thresh;
-	for (i = 0; i < size; i++) {
-		if (data[i] >= thresh) {
-			if (data[i] <= r) {
-				r = data[i];
-			}
-			else {
-				r = thresh;
-				peaks[count++] = i-1;
-			}
-		}
+
+	for (i = 1; i < size-1; i++) {
+		if (data[i] >= thresh && data[i-1] < data[i] && data[i+1] < data[i])
+			peaks[count++] = i+1;
 	}
+	return count;
 }
--- lab6/test.c
+++ lab6/test.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #include "ece486_fft.h"
 
-int main(char argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	float data[] = {1.,2.,3.,4.,5.,6.,7.,8.,9.,10.,9.,8.,7.,6.,5.,4.,3.,2.,4.,5.,6.,7.,6.,5.,4.};
+	float data[] = {1.,2.,3.,4.,5.,6.,7.,8.,9.,10.,9.,8.,9.,6.,5.,4.,3.,2.,4.,5.,6.,7.,6.,5.,4.};
 	int size = 25;
 	float thresh = 3.;
-	int peaks[] = {0,0,0,0,0,0};
+	int peaks[] = {0,0,0,0,0,0,0,0,0,0};
 
 	int ret;
 	ret = peak_detect(data, size, thresh, peaks);
 
 	printf("%d peaks found.\n", ret);
 	printf("Peaks found at: ");
-	for (ret = 0; ret < 6; ret++) {
-		if (peaks[ret])
-			printf("%d", peaks[ret]);
+	for (ret = 0; ret < 10; ret++) {
+		if (peaks[ret]) printf("%d\n", peaks[ret]);
 	}
-
-
 
 	return 0;
 }
