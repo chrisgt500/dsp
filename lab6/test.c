3a4a491d4ded5064b970d7ec243a58c64ea0c477
Forrest Smith <fwsmith898@gmail.com>
2016-04-25 17:26:47 -0400

    filter stuff

--- lab6/test.c
+++ lab6/test.c
@@ -20,17 +20,30 @@
 #include "stm32l4xx_hal.h"
 #include "stm32l476g_discovery.h"
 #include "ece486_fft.h"
+#include "ece486_biquad.h"
 
 
 extern FlagStatus KeyPressed;
 
 int main(int argc, char *argv[])
 {
-	float *input1, *input2;
+	BIQUAD_T *filter1;
+	int sections1, blocksizelpf;
+	float *input1, *input2, gain1;
 	char lcd_str[8] = {0};
 	float *peak_index;
 	peak_index = malloc(sizeof(float));
 	*peak_index = 0;
+	sections1 = 3;
+	blocksizelpf = 96;
+	gain1 = .000436;
+
+
+	float lpf1[15] = {
+		1.000000, -1.711824, 1.000000, -1.894179, 0.960442,
+		1.000000, -1.365929, 1.000000, -1.855035, 0.885547,
+		1.000000, 1.000000, 0.000000, -0.921111, 0.000000
+	};
 
 	setblocksize(FFTSAMPLES); //FUN FACT, THIS NEEDS TO BE CALLED BEFORE initialize
 	initialize(FS_48K, STEREO_IN, MONO_OUT);
@@ -43,6 +56,8 @@ int main(int argc, char *argv[])
 		flagerror(MEMORY_ALLOCATION_ERROR);
 		while(1);
 	}
+
+	filter1 = init_biquad(sections1, gain1, lpf1, blocksizelpf);
 
 	while(1){
 		getblockstereo(input1,input2);
