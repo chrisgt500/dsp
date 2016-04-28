7b72031ced753b293f56f208722da1ce6bbbe4da
Sean Turner <sean.turner1@maine.edu>
2016-04-28 12:12:20 -0400

    made things pretty

--- lab6/test.c
+++ lab6/test.c
@@ -50,7 +50,6 @@ int main(int argc, char *argv[])
 	s->pState = state;
 	s1->pState = state1;
 
-
 	float coefs[55] = {
 	0.000571, 0.000743, 0.001066, 0.001301, 0.001319, 0.000965, 0.000088, -0.001432,
 	-0.003647, -0.006511, -0.009848, -0.013343, -0.016548, -0.018908, -0.019816,
@@ -60,20 +59,9 @@ int main(int argc, char *argv[])
 	-0.018908, -0.016548, -0.013343, -0.009848, -0.006511, -0.003647, -0.001432, 0.000088,
 	0.000965, 0.001319, 0.001301, 0.001066, 0.000743, 0.000571
 	};
-
-	float coefs1[55] = {
-	0.000571, 0.000743, 0.001066, 0.001301, 0.001319, 0.000965, 0.000088, -0.001432,
-	-0.003647, -0.006511, -0.009848, -0.013343, -0.016548, -0.018908, -0.019816,
-	-0.018679, -0.014995, -0.008436, 0.001084, 0.013363, 0.027890, 0.043871, 0.060279,
-	0.075951, 0.089696, 0.100417, 0.107233, 0.109571, 0.107233, 0.100417, 0.089696, 0.075951,
-	0.060279, 0.043871, 0.027890, 0.013363, 0.001084, -0.008436, -0.014995, -0.018679, -0.019816,
-	-0.018908, -0.016548, -0.013343, -0.009848, -0.006511, -0.003647, -0.001432, 0.000088,
-	0.000965, 0.001319, 0.001301, 0.001066, 0.000743, 0.000571
-	};
-
-
+	
 	s->pCoeffs = coefs;
-	s1->pCoeffs = coefs1;
+	s1->pCoeffs = coefs;
 
 	setblocksize(blocksizelpf); //FUN FACT, THIS NEEDS TO BE CALLED BEFORE initialize
 	initialize(FS_48K, STEREO_IN, STEREO_OUT);
@@ -82,16 +70,13 @@ int main(int argc, char *argv[])
 	input2 = (float *)malloc(sizeof(float)*blocksizelpf);
 	input_decimated_1 = (float *)malloc(sizeof(float)*(blocksizelpf/decimation));
 	input_decimated_2 = (float *)malloc(sizeof(float)*(blocksizelpf/decimation));
-	//buffer = (float *)malloc(sizeof(float)*FFTSAMPLES*2);
-
 
 	if (input1==NULL || input2==NULL) {
 		flagerror(MEMORY_ALLOCATION_ERROR);
 		while(1);
 	}
 
-
-	do{
+	while(1){
 
 		if (KeyPressed) {
       		KeyPressed = RESET;
@@ -99,17 +84,12 @@ int main(int argc, char *argv[])
 			BSP_LED_Toggle(LED4);
 		}
 
-
 		for( j = 0; j < 8; j++){
-
 			getblockstereo(input1,input2);
 
-			//unsure if we need two seperate arm_fir_decimate_instance_f32's for filtering or just one
 			arm_fir_decimate_f32(s1, input2, input_decimated_2, blocksizelpf);
 			arm_fir_decimate_f32(s, input1, input_decimated_1, blocksizelpf);
 
-
-			// (blocksizelpf/decimation)
 			for(i = 0; i < (FFTSAMPLES)/8; i++){
 				buffer[2*i+j*256] = input_decimated_1[i];
 				buffer[2*i+j*256+1] = input_decimated_2[i];
@@ -121,13 +101,6 @@ int main(int argc, char *argv[])
 
 		velocity_conversion_display(peak_index, button_flag);
 
-		if (button_flag == 1) printf("FORWARDS %f \n", *peak_index);
-		if (button_flag == -1) printf("Backwards %f \n", *peak_index);
-
-
-
-	}while(1);
-
+	}
 	return 0;
-
 }
