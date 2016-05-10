f1518f08918c9549cd2651d17150a88e026266d7
Colin Leary <colin.leary@maine.edu>
2016-05-10 09:43:30 -0400

    Added correct parks mclellan script

---
+++ scripts/parks_mclellan.m
@@ -0,0 +1,33 @@
+M = 120;        %Number of filter coefficients: Adjust to meet spec
+
+%Desired response: Spec three separate bands by giving the corner
+%frequencies and gains at the corner frequencies. Frequencies are 
+%normalized by Fs/2. Gains are absolute values (not dB)
+A1 = 10^(12/20);        %passband gain
+F = [0 0.125 0.15 0.25 0.28 0.5]/0.5;       %corner freq
+A = [0 0     A1   A1   0    0];             %corner freq gains
+
+%weight each vector designed so that each band has its own tolerance
+d1 = 10^(12/20)-10^(11.75/20);      %max passband error
+d2 = 10^(-70/20);                   %max stopband error
+W  = [1/d2 1/d1 1/d2];           %weight per specified band
+
+%filter design and some really ugly plot code intermixed
+
+stop = 20*log10(d2);
+passh = 20*log10(A1+d1);
+passl = 20*log10(A1-d1);
+
+figure(1); clf;
+hold on;
+patch([F(1)*.5 F(2)*.5 F(2)*.5 F(1)*.5],[stop stop stop-20 stop-20], .9*[1 1 1]);
+patch([F(3)*.5 F(4)*.5 F(4)*.5 F(3)*.5], [passh passh passl passl], .9*[1 1 1]);
+patch([F(5)*.5 F(6)*.5 F(6)*.5 F(5)*.5], [stop stop stop-20 stop-20], .9*[1 1 1]);
+
+
+h = firpm(M-1, F, A, W);        %this is the filter creation
+r= freqz(h,1);                  %for plotting
+
+q = 0:0.5/length(r):0.5-0.5/length(r);
+plot(q,20*log10(abs(r)));
+axis([0 .5 -90 20]);
